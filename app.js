ebd53d3cf207aabd4a24ea1ed2ad9f18312c2d4d
John Ross <johnross@Johns-MacBook-Pro.local>
2018-08-14 21:14:57 -0500

    modified lookup endpoint to proxy call to walmart item api

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const lookupRouter = require('./routes/lookup');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, User-Agent");
   next();
 });
 
--- routes/lookup.js
+++ routes/lookup.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch')
 
-/* POST A UPC IN AN ATTEMPT TO RETRIEVE NAME. */
-// Accepts: { "upc": string }
-// Returns: results JSON
-// Results include: Name, Description, Brand, Price, etc.
-router.post('/', function(req, res, next) {
-  const lookupURL = 'https://api.upcitemdb.com/prod/trial/lookup';
-  fetch(lookupURL, {
-    method: 'POST',
-    body: JSON.stringify(req.body)
-  }).then(res => res.json())
+/* RETRIEVE ITEM DETAILS USING WALMART API KEY AND UPC */
+router.get('/:apiKey/:upc', function(req, res, next) {
+  const lookupURL = `https://api.walmartlabs.com/v1/items?apiKey=${req.params.apiKey}&upc=${req.params.upc}`;
+  fetch(lookupURL).then(res => res.json())
   .catch(error => res.send({ 'error': error }))
   .then(response => res.send(response));
 });
